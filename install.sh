43dcb285f786eb944a72ae40d85058dd7044a6b8
Olaf Czarnecki <olaf.czarnecki@outlook.com>
2022-02-10 18:46:40 +0000

    Added logind.conf
    Suspend on power key instead of shutting down immediately.

--- install.sh
+++ install.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/bash
 
+if [ "$EUID" -ne 0 ] ; then
+  echo "Please run as root. Need to install global system configuration"
+  exit 1
+fi
+
+SRC_DIR=`pwd`
+
 # nvim
 mkdir -p ~/.config/nvim/
-ln -fbs "$HOME/develop/dotfiles/init.vim" ~/.config/nvim/init.vim
+ln -fbs "$SRC_DIR/init.vim" ~/.config/nvim/init.vim
 
 # zsh
-ln -fbs "$HOME/develop/dotfiles/zshrc" ~/.zshrc
+ln -fbs "$SRC_DIR/zshrc" ~/.zshrc
+
+# logind.conf
+ln -fbs "$SRC_DIR/logind.conf" "/etc/systemd/logind.conf"
