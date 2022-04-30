9d7a77fcb8179a7831d7ea9ecd7977f650471d43
Olaf Czarnecki <olaf.czarnecki@outlook.com>
2022-04-30 10:37:33 +0100

    Added: .XCompose
    Greek letters (e.g. <compose> <g> <a> -> α)

--- install.sh
+++ install.sh
@@ -1,25 +1,30 @@
 #!/usr/bin/bash
 
-if [ "$EUID" -ne 0 ] ; then
-  echo "Please run as root. Need to install global system configuration"
-  exit 1
-fi
-
 SRC_DIR=`pwd`
 
 # nvim
 mkdir -p ~/.config/nvim/
 ln -fbs "$SRC_DIR/init.vim" ~/.config/nvim/init.vim
 
+# XCompose
+ln -fbs "$SRC_DIR/XCompose" ~/.XCompose
+
 # zsh
 ln -fbs "$SRC_DIR/zshrc" ~/.zshrc
 
+echo "User configuration installed!"
+
 ### Global system configuration.
 
-# faillock.conf
-chown root:root "$SRC_DIR/faillock.conf"
-ln -fbs "$SRC_DIR/faillock.conf" "/etc/security/faillock.conf"
+if [ "$EUID" -eq 0 ] ; then
+  # faillock.conf
+  chown root:root "$SRC_DIR/faillock.conf"
+  ln -fbs "$SRC_DIR/faillock.conf" "/etc/security/faillock.conf"
 
-# logind.conf
-chown root:root "$SRC_DIR/logind.conf"
-ln -fbs "$SRC_DIR/logind.conf" "/etc/systemd/logind.conf"
+  # logind.conf
+  chown root:root "$SRC_DIR/logind.conf"
+  ln -fbs "$SRC_DIR/logind.conf" "/etc/systemd/logind.conf"
+else
+  echo "Not running as root, skipping global system configuration."
+  exit 1
+fi
